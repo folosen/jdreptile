22344371c52ce2463251e43caa106aba33e93fb7
folosen <frozenyihan@sina.cn>
2017-06-11 23:00:21 +0800

    selenium

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
 var request = require("request");
+var fs = require("fs");
+var cheerio = require("cheerio");
+var superagent = require("superagent");
+var webdriver = require('selenium-webdriver');
 
-var options = { method: 'POST',
-  url: 'http://localhost:8000/doload',
-  headers: 
-   { 'content-type': 'application/x-www-form-urlencoded',
-     'postman-token': '6d1bc037-3f74-6a2c-d3da-426e2070bc5a',
-     'cache-control': 'no-cache',
-     'accept-charset': 'UTF-8' },
-  form: 
-   { url: 'https://passport.jd.com/uc/login',
-     renderTime: '6000',
-     script: '$("#loginname").val("seimimaster");$("#nloginpwd").val("seimi");$(".login-btn>a").click();',
-     contentType: 'img',
-     useCookie: '1' } };
+var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  //body 为图片文件流，自行处理
-});
+driver.get('https://passport.jd.com/new/login.aspx')
+.then(function(){
+    driver.findElement(webdriver.By.className('login-tab-r')).click();
+})
+.then(function(){
+    driver.findElement(webdriver.By.id('loginname')).sendKeys("18659903982");
+})
+.then(function(){
+    driver.findElement(webdriver.By.id('nloginpwd')).sendKeys("folosen$30821");
+})
+.then(function(){
+    driver.findElement(webdriver.By.id("loginsubmit")).click();
+})
+.then(function(){
+    sleep(3000);
+    driver.findElement(webdriver.By.id("ttbar-login")).click();
+})
+.then(function(){
+    sleep(3000);
+    console.log("/////////////////");
+    driver.
+    //driver.findElement(webdriver.By.id("ttbar-login"));
+})
+// function buildCookieMap(cookies) {
+//     var map = {};
+//     cookies.forEach(function (cookie) {
+//         map[cookie.name] = cookie;
+//     });
+//     return map;
+// }
+function sleep(d) {
+    for (var t = Date.now(); Date.now() - t <= d;);
+}
+
+//driver.quit();
+// var options = { method: 'POST',
+//   url: 'http://localhost:8000/doload',
+//   headers: 
+//    { 'content-type': 'application/x-www-form-urlencoded',
+//      'postman-token': '6d1bc037-3f74-6a2c-d3da-426e2070bc5a',
+//      'cache-control': 'no-cache',
+//      'accept-charset': 'UTF-8' },
+//   form: 
+//    { url: 'https://passport.jd.com/uc/login',
+//      renderTime: '6000',
+//      script: '$("#loginname").val("seimimaster");$("#nloginpwd").val("seimi");$(".login-btn>a").click();',
+//      contentType: 'img',
+//      useCookie: '1' } };
+
+// request(options, function (error, response, body) { 
+//   if (error) throw new Error(error);
+//   //body 为图片文件流，自行处理
+// });
+// superagent
+//   .get('https://www.pixiv.net/')
+//   .end(function(err, res){
+//     $ = cheerio.load(res.text);
+//     // console.log($("#wrapper").html());
+    
+//     fs.writeFile("pixiv.txt", res.text, function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }        
+//     });
+//   });
