3b98355618d8644c6ec5e63af4f960be196940a4
folosen <frozenyihan@sina.cn>
2017-06-20 10:40:35 +0800

    commonjs规范
    Signed-off-by: folosen <frozenyihan@sina.cn>

--- nodeDemo.js
+++ nodeDemo.js
@@ -1,4 +1,3 @@
-var 
 // try{
 //     process.nextTick(function () {
 //         throw new Error("error");
@@ -6,11 +5,54 @@ var
 // } catch (err) {
 //     console.log(err);
 // }
-try {
-  setTimeout(function(){
-    throw new Error("error");
-  },1)
-} catch (err) {
-  //can not catch it
-  console.log(err);
-}
+// try {
+//   setTimeout(function(){
+//     throw new Error("error");
+//   },1)
+// } catch (err) {
+//   //can not catch it
+//   console.log(err);
+// }
+// function async2(continuation){
+//   setTimeout(function(){
+//     try {
+//       var res = 42;
+//       if(false)
+//         throw new Error("woops!")
+//       else
+//         continuation(null,res);
+//     } catch (error) {
+//       continuation(error,null);
+//     }
+//   })
+// };
+// async2(function(err,res){
+//   if(err)
+//     console.log("err:",err);
+//   else
+//     console.log("cps:",res)
+// })
+// var EventEmitter = require('events').EventEmitter;
+// var emitter = new EventEmitter();
+// emitter.emit('error', new Error('something bad happened'));
+
+// emitter.on('error',function(err){
+//   console.error('出错：'+err.message)
+// })
+// var logger = require('tracer').console();
+// process.on('uncaughtException', function(err) {
+//   console.error('Error caught in uncaughtException event:', err);
+// });
+
+// try {
+//   setTimeout(function(){
+//     throw new Error("error");
+//   },1);
+// } catch (err) {
+//   //can not catch it
+//   console.log(err);
+// }
+var demo = require("demo");
+console.log(demo.x);
+console.log(demo.addX(5));
+
